e2b888470c58d6d459040a83a2f27b2c8e378405
melinamontelongo <melinamontelongo97@gmail.com>
2022-08-13 18:13:07 -0300

    Autenticación con Google

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 let email = document.getElementById("form-email");
 let password = document.getElementById("form-password");
 let form = document.getElementById("login-form");
+let googleSignIn = document.getElementById("g-signin2");
+let googleSignOut = document.getElementById("g-signout")
 //Se obtienen los elementos necesarios y se almacenan en variables
 
 form.addEventListener("submit", function(e){  //Escucha de eventos para cuando se envíe el formulario
@@ -13,7 +15,33 @@ form.addEventListener("submit", function(e){  //Escucha de eventos para cuando s
     document.getElementById("password-error").style.display = "block"   //mismo proceso que con la validación de e-mail.
     password.classList.replace("form-input", "input-error");           
 } else if (email.value !== "" && password.value !== ""){            //Se chequea que estén ambos campos
-    sessionStorage.setItem("logged_in", true);                     //Asigna un par de valores de session storage, válidos hasta que se cierre la pestaña
-    window.location.href = "index.html";                          //Si están los dos, se carga la portada del sitio.
+    sessionStorage.setItem("logged_in", true);                     //Asigna un par de valores de session storage y
+    window.location.href = "index.html";                          //se carga la portada del sitio.
 }
 })
+
+function handleCredentialResponse(response) {   //Recibe las credenciales de usuario en jwt y
+ const data = jwt_decode(response.credential); //las decodifica con la biblioteca
+ console.log(data);             
+ if (data.email_verified){                    //accede a la propiedad del objeto, y si es true
+  sessionStorage.setItem("logged_in", true)  //asigna valores de session storage para identificar que el usuario ya inició sesión y
+  window.location.href = "index.html";       //redirecciona a la portada.
+}
+}
+
+window.onload = function () {            //Cuando carga
+  google.accounts.id.initialize({       //inicializa el cliente de acceder con Google
+    client_id: "381953423886-47c5ne9hmniom37j9qduls0fgt35sa8b.apps.googleusercontent.com", //client id creado con la consola de google developers
+    callback: handleCredentialResponse  //llama a la función que maneja las credenciales de usuario
+  });
+  google.accounts.id.renderButton(       //Configura el botón de Google
+    document.getElementById("googleBtn"),
+    { theme: "filled_blue",
+    size: "large" ,
+    type:"standard",
+    shape:"pill",
+    text:"$ {button.text}",
+    logo_alignment:"center"
+  }  
+  );
+}
