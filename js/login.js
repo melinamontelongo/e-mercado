cf3ebbf834791f0088d2e924ef0cbc3c313e47eb
melinamontelongo <melinamontelongo97@gmail.com>
2022-09-10 14:04:58 -0300

    Estilo y validación de login con bootstrap

--- js/login.js
+++ js/login.js
@@ -1,33 +1,11 @@
-let email = document.getElementById("form-email");
-let password = document.getElementById("form-password");
-let form = document.getElementById("login-form");
-let check = document.getElementById("form-check");
 //Se obtienen los elementos necesarios y se almacenan en variables
-
-form.addEventListener("submit", function (e) {  //Escucha de eventos para cuando se envíe el formulario
-  e.preventDefault();                        //Se previene la acción por defecto (subir el formulario)
-  if (email.value === "") {                    //Validación de e-mail equivalente al required(no tiene que estar vacío)
-    document.getElementById("email-error").style.display = "block";  //Si está vacío, se muestra el mensaje de error...
-    email.classList.replace("form-input", "input-error");           //y se remplaza la clase del input por otra que tiene el estilo para marcar el error.
-  }
-  if (password.value === "") {                                             //Validación de contraseña
-    document.getElementById("password-error").style.display = "block"   //mismo proceso que con la validación de e-mail.
-    password.classList.replace("form-input", "input-error");
-  }
-  if (email.value !== "" && password.value === "") {                  //Si uno de los inputs (email) está correcto y el otro no,  
-    document.getElementById("email-error").style.display = "none"     //se le quita el estilo de error al correcto
-    email.classList.replace("input-error", "form-input");
-  }
-  if (password.value !== "" && email.value === "") {                   //Lo mismo pero con la contraseña        
-    document.getElementById("password-error").style.display = "none"
-    password.classList.replace("input-error", "form-input");
-
-  } else if (email.value !== "" && password.value !== "") {         //Se chequea que estén ambos campos
-    loginSession(email.value);
-  }
-})
+let email = document.getElementById("form-email");
+let check = document.getElementById("form-check");
+let form = document.getElementById("login-form");
 
 //Función para permitirle al usuario decidir si mantener iniciada la sesión (localStorage) o no (sessionStorage)
+  //Almacena el dato que se le pase
+  //Redirecciona
 function loginSession(userData){
   if (check.checked) {
     localStorage.setItem("user", userData);
@@ -37,13 +15,13 @@ function loginSession(userData){
   window.location.href = "index.html";    
 }
 
+//Función que gestiona la respuesta que devuelve el acceso con Google
 function handleCredentialResponse(response) {   //Recibe las credenciales de usuario en jwt y
   const data = jwt_decode(response.credential); //las decodifica con la biblioteca
   if (data.email_verified) {                    //accede a la propiedad del objeto, y si es true
     loginSession(data.email);                   //guarda el dato en almacenamiento
   }
 }
-
 window.onload = function () {            //Cuando carga
   google.accounts.id.initialize({       //inicializa el cliente de acceder con Google
     client_id: "381953423886-47c5ne9hmniom37j9qduls0fgt35sa8b.apps.googleusercontent.com", //client id creado con la consola de google developers
@@ -52,12 +30,22 @@ window.onload = function () {            //Cuando carga
   google.accounts.id.renderButton(       //Configura el botón de Google
     document.getElementById("googleBtn"),
     {
-      theme: "filled_blue",
-      size: "large",
+      theme: "filled_black",
+      size: "medium",
       type: "standard",
-      shape: "pill",
+      shape: "rectangular",
       text: "$ {button.text}",
-      logo_alignment: "center"
+      logo_alignment: "left",
+      width: 10,     
     }
   );
+
 }
+document.addEventListener("DOMContentLoaded", function(){
+  form.addEventListener('submit', event => {  //cuando se "envíe" el formulario
+    event.preventDefault();     
+    form.classList.add('was-validated')  //agrega las clases de validación de bootstrap
+    if (form.checkValidity()) {         //valida lo ingresado por el usuario (que no sean campos vacíos)
+      loginSession(email.value);        //Llama a la función que almacena el usuario en almacenamiento local
+    }})  
+})
